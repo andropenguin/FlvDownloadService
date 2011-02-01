0f341781b387f251eace590863de822d193bd02b
andropenguin <andropenguin@gmail.com>
2011-02-01 09:24:21 +0900

    upgrade Android NDK from r4b to r5b beacuse several files cannot be downloaded continuously when using Android SDK r09 and Eclipse ADT 9.

--- obj/local/armeabi/objs/flvstreamer/com_sarltokyo_flvdownloadservice_FlvDownloadService.o.d
+++ obj/local/armeabi/objs/flvstreamer/com_sarltokyo_flvdownloadservice_FlvDownloadService.o.d
@@ -2,70 +2,76 @@
  /home/osabe/workspace/FlvDownloadService/jni/com_sarltokyo_flvdownloadservice_FlvDownloadService.c \
  /home/osabe/workspace/FlvDownloadService/jni/flvstreamer.h \
  /home/osabe/workspace/FlvDownloadService/jni/com_sarltokyo_flvdownloadservice_FlvDownloadService.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/jni.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdlib.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/string.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/malloc.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/alloca.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/strings.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdint.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/_types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/_types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/posix_types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/stddef.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/compiler.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/posix_types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/kernel.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/memory.h
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/jni.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/stdlib.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/cdefs.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/string.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/malloc.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/alloca.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/strings.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/stdint.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/_types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/machine/_types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/posix_types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/stddef.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/compiler.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/asm/posix_types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/asm/types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/machine/kernel.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/memory.h \
+ /home/osabe/workspace/FlvDownloadService/jni/log.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/stdio.h
 
 /home/osabe/workspace/FlvDownloadService/jni/flvstreamer.h:
 
 /home/osabe/workspace/FlvDownloadService/jni/com_sarltokyo_flvdownloadservice_FlvDownloadService.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/jni.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/jni.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdlib.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/stdlib.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/cdefs.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/string.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/string.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/malloc.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/malloc.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/alloca.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/alloca.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/strings.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/strings.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdint.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/stdint.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/_types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/_types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/_types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/machine/_types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/posix_types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/posix_types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/stddef.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/stddef.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/compiler.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/compiler.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/posix_types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/asm/posix_types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/asm/types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/kernel.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/machine/kernel.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/memory.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/memory.h:
+
+/home/osabe/workspace/FlvDownloadService/jni/log.h:
+
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/stdio.h:
--- obj/local/armeabi/objs/flvstreamer/log.o.d
+++ obj/local/armeabi/objs/flvstreamer/log.o.d
@@ -1,65 +1,65 @@
 /home/osabe/workspace/FlvDownloadService/obj/local/armeabi/objs/flvstreamer/log.o: \
  /home/osabe/workspace/FlvDownloadService/jni/log.c \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdio.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/_types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/_types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdint.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/posix_types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/stddef.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/compiler.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/posix_types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/types.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/kernel.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/string.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/malloc.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/assert.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/ctype.h \
- /home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/android/log.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/stdio.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/cdefs.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/_types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/machine/_types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/stdint.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/posix_types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/stddef.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/compiler.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/asm/posix_types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/asm/types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/types.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/machine/kernel.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/string.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/malloc.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/assert.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/ctype.h \
+ /home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/android/log.h \
  /home/osabe/workspace/FlvDownloadService/jni/log.h
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdio.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/stdio.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/cdefs.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/_types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/_types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/_types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/machine/_types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdint.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/stdint.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/posix_types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/posix_types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/stddef.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/stddef.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/compiler.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/compiler.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/posix_types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/asm/posix_types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/asm/types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/types.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/linux/types.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/kernel.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/machine/kernel.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/string.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/string.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/malloc.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/malloc.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/assert.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/assert.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/ctype.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/ctype.h:
 
-/home/osabe/src/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/android/log.h:
+/home/osabe/src/android-ndk-r5b/platforms/android-4/arch-arm/usr/include/android/log.h:
 
 /home/osabe/workspace/FlvDownloadService/jni/log.h:
